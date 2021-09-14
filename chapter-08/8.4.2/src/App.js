122a0fb416dd0da77069406469e07b3d16bc3d46
takahashikazunari <kamenaris@gmail.com>
2021-09-14 10:27:41 +0900

    react-windowのFixedSizeListを使って、仮想リストを実装

--- chapter-08/8.4.2/src/App.js
+++ chapter-08/8.4.2/src/App.js
@@ -1,34 +1,30 @@
 import React from "react";
+import { FixedSizeList } from "react-window"
+import faker from "faker";
 
-const tahoe_peaks = [
-  { name: "Freel Peak", elevation: 10891 },
-  { name: "Monument Peak", elevation: 10067 },
-  { name: "Pyramid Peak", elevation: 9983 },
-  { name: "Mt. Tallac", elevation: 9735 }
-];
-
-function List({ data = [], renderItem, renderEmpty }) {
-  return !data.length ? (
-    renderEmpty
-  ) : (
-    <ul>
-      {data.map((item, i) => (
-        <li key={i}>{renderItem(item)}</li>
-      ))}
-    </ul>
-  );
-}
+const bigList = [...Array(5000)].map(() => ({
+  name: faker.name.findName(),
+  email: faker.internet.email(),
+  avatar: faker.internet.avatar()
+}))
 
 export default function App() {
+  const renderRow = (({ index, style }) => (
+    <div style={{ ...style, ...{ display: "flex" } }}>
+      <img src={bigList[index].avatar} alt={bigList[index].name} width={50} />
+      <p>
+        {bigList[index].name} - {bigList[index].email}
+      </p>
+    </div>
+  ));
   return (
-    <List 
-      data={tahoe_peaks}
-      renderEmpty={<p>This list is empty</p>}
-      renderItem={item => (
-        <>
-          {item.name} - {item.elevation.toLocaleString()}
-        </>
-      )} 
-    />
+    <FixedSizeList 
+      height={window.innerHeight}
+      width={window.innerWidth - 20}
+      itemCount={bigList.length}
+      itemSize={50}
+    >
+      {renderRow}
+    </FixedSizeList>
   )
 }
