2d85fe3e46d4a10d2a1554c77a917796468adda5
takahashikazunari <kamenaris@gmail.com>
2021-09-18 14:54:06 +0900

    GitHubUserをコンポーネント化、useFetchをカスタムフックにする

--- chapter-08/8.4.3/src/App.js
+++ chapter-08/8.4.3/src/App.js
@@ -1,30 +1,9 @@
-import React from "react";
-import { FixedSizeList } from "react-window"
-import faker from "faker";
-
-const bigList = [...Array(5000)].map(() => ({
-  name: faker.name.findName(),
-  email: faker.internet.email(),
-  avatar: faker.internet.avatar()
-}))
+import React, { useState } from "react";
+import { GithubUser } from "./GithubUser";
 
 export default function App() {
-  const renderRow = (({ index, style }) => (
-    <div style={{ ...style, ...{ display: "flex" } }}>
-      <img src={bigList[index].avatar} alt={bigList[index].name} width={50} />
-      <p>
-        {bigList[index].name} - {bigList[index].email}
-      </p>
-    </div>
-  ));
+  const [login, setLogin] =useState("moonhighway");
   return (
-    <FixedSizeList 
-      height={window.innerHeight}
-      width={window.innerWidth - 20}
-      itemCount={bigList.length}
-      itemSize={50}
-    >
-      {renderRow}
-    </FixedSizeList>
-  )
+    <GithubUser login={login} />
+  );
 }
