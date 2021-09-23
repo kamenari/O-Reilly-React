77a3eb1670d30ecb9946204f82f97b6581952513
takahashikazunari <kamenaris@gmail.com>
2021-09-23 14:29:17 +0900

    イテレーターのカスタムフックを作成、メモ化 リポジトリ名の取得、前のリポジトリ名と次のリポジトリ名を描画するコンポーネント作成 Fetchコンポーネントを使って、リポジトリ名を取得して描画

--- chapter-08/8.5.1/src/App.js
+++ chapter-08/8.5.1/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { GitHubUser } from "./GithubUser";
 
 export default function App() {
-  const [login, setLogin] =useState("moonhighway");
+  const [login, setLogin] =useState("kamenari");
   return (
     <GitHubUser login={login} />
   );
