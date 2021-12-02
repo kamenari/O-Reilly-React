346ba71f2a58c4facbfac39ee24d7ea204c37a38
takahashi kazunari <kamenaris@gmail.com>
2021-12-02 10:25:24 +0900

    GraphQLでgithubの情報を取得する(githubのtokenが通らなくて読み込めない)

--- chapter-08/8.6.2/src/App.js
+++ chapter-08/8.6.2/src/App.js
@@ -1,25 +1,77 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SearchForm from "./SearchForm";
+import { GraphQLClient } from 'graphql-request';
+
+const query = `
+  query findRepos($login:String!) {
+    user(login:$login) {
+      login
+      name
+      location
+      avatar_url: avatarUrl
+      repositories(first:100) {
+        totalCount
+        nodes {
+          name
+        }
+      }
+    }
+  }
+`;
+
+const client = new GraphQLClient( "https://api.github.com/graphql", {
+  headers: {
+    Authorization: `Bearer <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>`
+  }
+});
+
+function UserDetails(data) {
+  return (
+    <div className="githubUser">
+      <img src={data.avatar_url} alt={data.login} style={{ width: 200 }}/>
+      <div>
+        <h1>{data.login}</h1>
+        {data.name && <p>{data.name}</p>}
+        {data.location && <p>{data.location}</p>}
+      </div>
+    </div>
+  )
+}
+
+function List({ data = [], renderItem, renderEmpty }) {
+  return !data.length ? (
+    renderEmpty
+  ) : (
+    <ul>
+      {data.map((item, i) => (
+        <li  key={i}>{renderItem(item)}</li>
+      ))}
+    </ul>
+  )
+}
 
 export default function App() {
-  const [login, setLogin] = useState();
-  const [repo, setRepo] = useState();
+  const [login, setLogin] = useState("moontahoe");
+  const [userData, setUserData] = useState();
 
-  const handleSearch = login => {
-    if (login) return setLogin(login);
-    setLogin(" ");
-    setRepo(" ");
-  }
+  useEffect(() => {
+    client
+    .request(query, { login })
+    .then(({ user }) => user)
+    .then(setUserData)
+    .catch(console.error);
+  }, [client, query, login]);
 
-  if (!login)
-    return (
-      <SearchForm value={login} onSearch={handleSearch} />
-    );
-
+  if (!userData) return <p>loading...</p>;
+  console.log(userData);
   return (
     <>
-      <SearchForm value={login} onSearch={handleSearch} />
-      
+      <SearchForm value={login} onSearch={setLogin} />
+        <UserDetails {...userData} />
+        <List 
+          data={userData.repositories.nodes}
+          renderItem={repo => <span>{repo.name}</span>}
+        />
     </>
     
   );
